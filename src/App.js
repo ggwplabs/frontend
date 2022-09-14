8d3475baf589c8950b37edcd411fdf620d93405a
Sergey Chudinov <schudinov@ggwp.world>
2022-09-14 16:20:18 +0700

    check phantom

--- src/App.js
+++ src/App.js
@@ -6,67 +6,78 @@ import CustomSelect from "./components/UI/select/CustomSelect";
 function App() {
     const [pubKey, setPubKey] = useState(null);
 
-    const getProvider = () => {
-        if ('phantom' in window) {
-            const provider = window.phantom?.solana;
+    // const getProvider = () => {
+    //     if ('phantom' in window) {
+    //         const provider = window.phantom?.solana;
+    //
+    //         if (provider?.isPhantom) {
+    //             return provider;
+    //         }
+    //     }
+    // };
 
-            if (provider?.isPhantom) {
-                return provider;
-            }
-        }
-        window.open('https://phantom.app/', '_blank');
-    };
+    const isPhantomInstalled = window.phantom?.solana?.isPhantom
 
-    const provider = getProvider();
+    // useEffect( () => {
+    //
+    //     provider.on("connect", (publicKey) => {
+    //         console.log('connect')
+    //         setPubKey(publicKey);
+    //     });
+    //
+    //     provider.on("disconnect", () => {
+    //         console.log('disconnect')
+    //         setPubKey(null);
+    //     });
+    //
+    //     provider.on('accountChanged', (publicKey) => {
+    //         setPubKey(publicKey);
+    //         provider.connect()
+    //     });
+    // }, [provider]);
 
-    useEffect( () => {
-
-        provider.on("connect", (publicKey) => {
-            console.log('connect')
-            setPubKey(publicKey);
-        });
-
-        provider.on("disconnect", () => {
-            console.log('disconnect')
-            setPubKey(null);
-        });
-
-        provider.on('accountChanged', (publicKey) => {
-            setPubKey(publicKey);
-            provider.connect()
-        });
-    }, [provider]);
-
-
-
-    if (pubKey) {
-        return (
-            <div>
-                <Tabs
-                    publicKey={pubKey}
-                />
-                <button
-                    style={{marginTop: 30}}
-                    onClick={async () => await provider.disconnect()}
-                > Logout
-                </button>
-            </div>
-        )
-    } else {
-        return (
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                }}
-            >
-                <button
+    if (isPhantomInstalled){
+        const provider = window.phantom?.solana;
+        if (pubKey) {
+            return (
+                <div>
+                    <Tabs
+                        publicKey={pubKey}
+                    />
+                    <button
+                        style={{marginTop: 30}}
+                        onClick={async () => await provider.disconnect()}
+                    > Logout
+                    </button>
+                </div>
+            )
+        } else {
+            return (
+                <div
                     style={{
-                        marginTop: 30}}
-                    onClick={async () => await provider.connect()}
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}
                 >
-                    Login with phantom
-                </button>
+                    <button
+                        style={{
+                            marginTop: 30}}
+                        onClick={async () => await provider.connect()}
+                    >
+                        Login with phantom
+                    </button>
+                </div>
+            )
+        }
+    } else {
+        return(
+            <div>
+                <a
+                    target={"_blank"}
+                    href={"https://phantom.app/"}
+                >
+                    Install phantom
+                </a>
             </div>
         )
     }
