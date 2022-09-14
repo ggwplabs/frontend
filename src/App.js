faa1ba6e84afb402985d4de4910e6bb02671cb17
Sergey Chudinov <schudinov@ggwp.world>
2022-09-14 22:11:39 +0700

    Release, no refactoring

--- src/App.js
+++ src/App.js
@@ -5,74 +5,81 @@ import CustomSelect from "./components/UI/select/CustomSelect";
 
 function App() {
     const [pubKey, setPubKey] = useState(null);
+    const isPhantomInstalled = window.phantom?.solana?.isPhantom
+
+    const getProvider = () => {
+        if ('phantom' in window) {
+            const provider = window.phantom?.solana;
+
+            if (provider?.isPhantom) {
+                return provider;
+            }
+        }
+    };
+
+    const provider = getProvider()
+
+    if (!isPhantomInstalled) {
+        return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+            For continuum, please install
+            <a
+                target={"_blank"}
+                href={"https://phantom.app/"}
+            >
+                 Phantom
+            </a>
+        </div>
+        )
+    }
+
+    if (pubKey) {
+        return (
+            <div>
+                <Tabs
+                    publicKey={pubKey}
+                />
+                <button
+                    style={{marginTop: 30}}
+                    onClick={async () => await provider.disconnect()}
+                > Logout
+                </button>
+            </div>
+        )
+    } else {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}
+            >
+                <button
+                    style={{
+                        marginTop: 30
+                    }}
+                    onClick={async () => await window.solana.connect()}
+                >
+                    Login with phantom
+                </button>
+            </div>
+        )
+    }
 
     return(
         <div>
-            test
+            <a
+                target={"_blank"}
+                href={"https://phantom.app/"}
+            >
+                Install phantom
+            </a>
         </div>
     )
-
-    // useEffect(() => {
-    //
-    //     window.solana.on("connect", (publicKey) => {
-    //         console.log('connect')
-    //         setPubKey(publicKey);
-    //     });
-    //
-    //     window.solana.on("disconnect", () => {
-    //         console.log('disconnect')
-    //         setPubKey(null);
-    //     });
-    //
-    //     window.solana.on('accountChanged', (publicKey) => {
-    //         setPubKey(publicKey);
-    //         window.solana.connect()
-    //     });
-    // }, [window.solana]);
-
-    // if (pubKey) {
-    //     return (
-    //         <div>
-    //             <Tabs
-    //                 publicKey={pubKey}
-    //             />
-    //             <button
-    //                 style={{marginTop: 30}}
-    //                 onClick={async () => await window.solana.disconnect()}
-    //             > Logout
-    //             </button>
-    //         </div>
-    //     )
-    // } else {
-    //     return (
-    //         <div
-    //             style={{
-    //                 display: 'flex',
-    //                 justifyContent: 'center'
-    //             }}
-    //         >
-    //             <button
-    //                 style={{
-    //                     marginTop: 30
-    //                 }}
-    //                 onClick={async () => await window.solana.connect()}
-    //             >
-    //                 Login with phantom
-    //             </button>
-    //         </div>
-    //     )
-    // }
-    //
-    // return(
-    //     <div>
-    //         <a
-    //             target={"_blank"}
-    //             href={"https://phantom.app/"}
-    //         >
-    //             Install phantom
-    //         </a>
-    //     </div>
-    // )
 }
 
 export default App;
